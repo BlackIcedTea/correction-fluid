77a14f71ad53e8c236a976120dbefd3408e64b76
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2016-03-22 23:29:16 +0800

    add RuleInlineCreator

--- chrome/extension/meatWagon.js
+++ chrome/extension/meatWagon.js
@@ -4,27 +4,67 @@ import createStore from '../../app/store/configureStore'
 
 const denyNodes = ['style'].map(x => x.toUpperCase())
 
+function replaceCorpse(rule, node) {
+  let find = xRegExp(rule.find, 'ig')
+  let replace = rule.replace
+  node.textContent = xRegExp.replace(node.textContent, find, replace)
+}
+
 function getAllTextNodes(node) {
-  if (node.nodeType === Node.TEXT_NODE &&
-    !denyNodes.includes(node.parentNode.nodeName)) {
+  if (node.nodeType === Node.TEXT_NODE) {
     return node
   }
   return _.flatMap(node.childNodes, getAllTextNodes)
+}
+
+function denyNodesFilter(denyNodeList) {
+  return ({ parentNode: { nodeName } }) => !denyNodeList.includes(nodeName)
+}
+
+function getAllTextNodesWithoutDenyNodes(node, denyNodeList) {
+  return getAllTextNodes(node).filter(denyNodesFilter(denyNodeList))
 }
 
 chrome.storage.local.get('state', obj => {
   const initialState = JSON.parse(obj.state || '{}')
   let state = createStore(initialState).getState()
   let rules = state.rules.filter(rule => xRegExp(rule.url).test(document.URL) && rule.isEnabled)
-  getAllTextNodes(document).forEach(node => {
-    rules.forEach(rule => {
-      if (Array.from(document.querySelectorAll(rule.selector))
-        .filter(parentNode => !['style'].map(x => x.toUpperCase()).includes(parentNode.nodeName))
-        .some(parentNode => parentNode.contains(node))) {
-        let find = xRegExp(rule.find, 'ig')
-        let replace = rule.replace
-        node.textContent = xRegExp.replace(node.textContent, find, replace)
-      }
-    })
+  getAllTextNodesWithoutDenyNodes(document).forEach(node =>
+    rules.filter(rule =>
+      _(document.querySelectorAll(rule.selector))
+        .filter(denyNodesFilter(denyNodes))
+        .value()
+        .some(parentNode => parentNode.contains(node))
+    ).forEach(rule => replaceCorpse(rule, node))
+  )
+
+  new MutationObserver(mutations => {
+    function characterData({ target }) {
+      let node = target
+      rules.filter(rule =>
+        _(document.querySelectorAll(rule.selector))
+          .filter(denyNodesFilter(denyNodes))
+          .value()
+          .some(parentNode => parentNode.contains(node))
+      ).forEach(rule => replaceCorpse(rule, node))
+    }
+
+    function childList({ target }) {
+      getAllTextNodesWithoutDenyNodes(target).forEach(node =>
+        rules.filter(rule =>
+          _(document.querySelectorAll(rule.selector))
+            .filter(denyNodesFilter(denyNodes))
+            .value()
+            .some(parentNode => parentNode.contains(node))
+        ).forEach(rule => replaceCorpse(rule, node))
+      )
+    }
+
+    _.each(mutations, mutation => ({ characterData, childList })[mutation.type](mutation))
+  })
+  .observe(document.body, {
+    characterData: true,
+    childList: true,
+    subtree: true
   })
 })
